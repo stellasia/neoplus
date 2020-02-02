7d073ca1edb4c4b27dfab21bd0decb701cf90113
stellasia <stellaou13@gmail.com>
2020-02-02 11:49:11 +0100

    Add procedure to add node property + fix tests

--- src/main/java/neoplus/LR.java
+++ src/main/java/neoplus/LR.java
@@ -3,6 +3,7 @@ package neoplus;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.procedure.*;
 
 import java.util.stream.Stream;
@@ -15,7 +16,9 @@ public class LR {
     @Context
     public GraphDatabaseService db;
 
-    /**First test procedure
+
+    /**First test procedure: multiply a node property by a given factor
+     * and streams the results
      *
      * @param label the label name to query by
      * @param property the property to be multiplied
@@ -32,6 +35,31 @@ public class LR {
         return db.findNodes(Label.label(label)).stream().map(
                 n -> new Result(n, property, factor)
         );
+    }
+
+    /**First test procedure: multiply a node property by a given factor
+     *
+     * @param label the label name to query by
+     * @param property the property to be multiplied
+     * @param factor the multiplication factor
+     * @return the nodes id + the multiplied property
+     */
+    @Procedure(name = "neoplus.multiplyByWrite", mode=Mode.WRITE)
+    @Description("Computes n.property*2 for all nodes with a given label")
+    public void multiplyByWrite(
+            @Name("label") String label,
+            @Name("property") String property,
+            @Name("factor") Double factor
+    ) {
+        Stream<Result> results = multiplyBy(label, property, factor);
+
+        try (Transaction tx = db.beginTx()) {
+            results.forEach(r -> {
+                Node n = db.getNodeById(r.nodeId);
+                n.setProperty("newValue", r.newValue);
+            });
+            tx.success();
+        }
     }
 
     public static class Result {
--- src/test/java/neoplus/LRTest.java
+++ src/test/java/neoplus/LRTest.java
@@ -1,6 +1,5 @@
 package neoplus;
 
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -9,6 +8,7 @@ import org.neo4j.harness.ServerControls;
 import org.neo4j.harness.TestServerBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.neo4j.driver.v1.Values.parameters;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -19,7 +19,6 @@ public class LRTest {
 
     @BeforeAll
     void initializeNeo4j() {
-
         this.embeddedDatabaseServer = TestServerBuilders
                 .newInProcessBuilder()
                 .withProcedure(LR.class)
@@ -27,22 +26,53 @@ public class LRTest {
     }
 
     @Test
-    public void shouldAllowIndexingAndFindingANode() {
+    public void shouldMultiplyBy() {
+
+        assertThat(false);
 
         // In a try-block, to make sure we close the driver and session after the test
         try(Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
             Session session = driver.session())
         {
-            // Given I've started Neo4j with the FullTextIndex procedure class
+            // Given I've started Neo4j with the neoplus procedure class
             //       which my 'neo4j' rule above does.
             // And given I have a node in the database
-            long nodeId = session.run( "CREATE (p:Label {property:1}) RETURN id(p)" )
+            long nodeId = session.run( "CREATE (p:Label {property:1.0}) RETURN id(p)" )
                     .single()
                     .get( 0 ).asLong();
 
-            // Then I can search for that node with lucene query syntax
-            StatementResult result = session.run( "CALL neoplus.multiplyBy(Label, 'property', 10)");
-            assertThat(result.single().get( "nodeId" ).asLong()).isEqualTo( nodeId );
+            // Then I can call the multiplyBy procedure
+            StatementResult result = session.run("CALL neoplus.multiplyBy('Label', 'property', 10.0)");
+            Record r = result.single();
+            assertThat(r.get( "nodeId" ).asLong()).isEqualTo( nodeId );
+            assertThat(r.get( "newValue" ).asDouble()).isEqualTo( 10.0 );
+        }
+    }
+
+    @Test
+    public void shouldMultiplyByWrite() {
+
+        // In a try-block, to make sure we close the driver and session after the test
+        try(Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
+            Session session = driver.session())
+        {
+            // Given I've started Neo4j with the neoplus procedure class
+            //       which my 'neo4j' rule above does.
+            // And given I have a node in the database
+            long nodeId = session.run( "CREATE (p:Label {property:1.0}) RETURN id(p)" )
+                    .single()
+                    .get( 0 ).asLong();
+
+            // Then I can call the multiplyByWrite procedure
+            session.run( "CALL neoplus.multiplyByWrite('Label', 'property', 10.0)");
+
+            // and it adds a new property 'newValue' to my graph
+            Value parameters = parameters(
+                    "nodeId", nodeId
+            );
+            StatementResult result = session.run(
+                    "MATCH (n:Label) WHERE id(n) = {nodeId} RETURN n.newValue as newValue",
+                    parameters);
             assertThat(result.single().get( "newValue" ).asDouble()).isEqualTo( 10.0 );
         }
     }
